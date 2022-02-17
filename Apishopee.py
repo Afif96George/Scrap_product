486efc262a6cd690b80fc9444c7894ef300cee73
afif <luqmanafif283@gmail.com>
2022-02-18 00:06:57 +0800

    chore: shopee fix

--- Apishopee.py
+++ Apishopee.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 Shopee_url = 'https://shopee.com.my'
-keyword_search = 'Nescafe Gold refill 170g'
+keyword_search = 'Longitech Camera'
 headers = {
  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
  "Referer": "{}search?keyword={}".format(Shopee_url, keyword_search)
@@ -13,8 +13,10 @@ r = requests.get(url, headers = headers).json()
 # Shopee scraping script
 titles_list = []
 prices_list = []
-# for item in r['items']:
-#     titles_list.append(item['name'])
-#     prices_list.append(item['price_min'])
+currencyMY  =  []
+for item in r['items']:
+    titles_list.append(item['name'])
+    prices_list.append(item['price_min'])
 Shopee = pd.DataFrame(zip(titles_list, prices_list), columns=["ItemName", "Price"])
+
 print(Shopee)
